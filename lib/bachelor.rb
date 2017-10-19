9c94a267d0a62e1b96ceebc3ad9fef6dc2699df4
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-19 16:02:42 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -49,7 +49,7 @@ def get_average_age_for_season(data, season)
   # code here
   age_array = []
   data[season].each do |key|
-    age.array.push(key["age"])
+    age_array.push(key["age"])
   end
   return age_array
 
