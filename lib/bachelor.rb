355e29472f4d356c7d5cc2f3d41756f3c6103250
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-19 15:35:30 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -17,8 +17,9 @@ def get_contestant_name(data, occupation)
   data.each do |season, contestant|
     if data[season][count]["occupation"] == occupation
       return "h"
-    else 
+    else
       return "h"
+    end
   end
 end
 
