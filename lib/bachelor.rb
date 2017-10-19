47d38ab1ba618fa60db48c389534786e9b051f64
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-19 15:21:33 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -13,6 +13,11 @@ end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |season, contestant|
+    season.each do |name, value|
+      return name
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
